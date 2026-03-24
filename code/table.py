1a99cc151e041d2dee659c477f08d48f9bfdaeb2
Sasha <sashacrawford91@gmail.com>
2026-03-23 21:29:07 -0400

    Renamed tables for DB accomodation

--- code/table.py
+++ code/table.py
@@ -1,6 +1,6 @@
-from database import load_data, tracker_table
+from database import netflix_table, tracker_table
 
-load_data() # load the data into the database
+netflix_table() # load the data into the database
 tracker_table() # create the tracker table if it doesn't exist
 
 print("Data loaded and tracker table created (if it didn't exist).")
