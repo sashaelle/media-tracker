6cd4a9591b19fe7e11f1ee31f86b8b1f03795aa2
Sasha <sashacrawford91@gmail.com>
2026-03-27 11:45:59 -0400

    Create tracked_title for saving tracked title to database

--- code/test.py
+++ code/test.py
@@ -1,6 +1,18 @@
-from media_search import search
+from tracked_title import track_title
+import sqlite3 as sql
 
-results = search("queen")
+results = track_title("s12", "The Office", "Watched", "2", "Great show!")
+
+conn = sql.connect('media_tracker.db')
+cursor = conn.cursor()
+
+cursor.execute(
+    """
+    SELECT *
+    FROM tracker
+    """,)
+    
+results = cursor.fetchall()
 
 for row in results:
     print(row)
