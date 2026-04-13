af87d93c7fd58be3c6474d154248c9647e5d857b
Sasha <sashacrawford91@gmail.com>
2026-04-13 12:46:56 -0400

    testing files and update db from dummy data

--- code/test.py
+++ code/test.py
@@ -1,10 +1,23 @@
-from tracked_title import track_title
+#import uuid
+#from tracked_title import track_title
 import sqlite3 as sql
 
-results = track_title("s12", "The Office", "Watched", "2", "Great show!")
+#results = track_title("s12", "The Office", "Watched", "2", "Great show!")
+
+#new_uuid = str(uuid.uuid1()) # Generate a unique identifier
+
+#tracked = "INSERT INTO account (account_id, name) " \
+#          "VALUES (?, ?)" \
+#          "ON CONFLICT (account_id)" \
+#          "DO UPDATE SET " \
+#          "     name=excluded.name"
+#entry_data = (new_uuid, "Crawford")
 
 conn = sql.connect('media_tracker.db')
 cursor = conn.cursor()
+
+#cursor.execute(tracked, entry_data)
+
 
 cursor.execute(
     """
