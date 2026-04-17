024647b58e7a825f959e1978794c24c71e01a8d9
Sasha <sashacrawford91@gmail.com>
2026-04-17 00:16:07 -0400

    Update track_title function to include profile and update SQL query for tracking titles

--- code/tracked_title.py
+++ code/tracked_title.py
@@ -1,25 +1,42 @@
 import sqlite3 as sql
 import uuid
 
-def track_title(media_ID, title, status, rating, review):
+def track_title(profile, media_id, title, status, rating, review):
     conn = sql.connect('media_tracker.db')
     cursor = conn.cursor()
 
     new_uuid = str(uuid.uuid4())
 
     tracked = """
-    INSERT INTO tracker (tracker_id, media_ID, title, status, rating, review)
-    VALUES (?, ?, ?, ?, ?, ?)
-    ON CONFLICT (media_ID)
+    INSERT INTO tracker (tracker_id, profile, media_id, title, status, rating, review)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+    ON CONFLICT (profile, media_id)
     DO UPDATE SET
         status=excluded.status,
         rating=excluded.rating,
         review=excluded.review
     """
 
-    entry_data = (new_uuid, media_ID, title, status, rating, review)
+    entry_data = (new_uuid, profile, media_id, title, status, rating, review)
 
     cursor.execute(tracked, entry_data)
     conn.commit()
     conn.close()
 
+def get_tracked_titles(profile):
+    conn = sql.connect('media_tracker.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT title
+        FROM tracker
+        WHERE profile = ?
+        ORDER BY title
+    """, (profile,))
+
+    rows = cursor.fetchall()
+    conn.close()
+    return [row[0] for row in rows]
+
+def modify_tracked_title(): 
+    return 0
