577f2e31c4b725e604d1d0f155312961bfe7371a
Sasha <sashacrawford91@gmail.com>
2026-04-17 17:16:00 -0400

    Create save_profile.py

---
+++ code/save_profile.py
@@ -0,0 +1,22 @@
+import sqlite3 as sql
+import uuid 
+
+def save_profile(name):
+    conn = sql.connect('media_tracker.db')
+    cursor = conn.cursor()
+
+    new_uuid = str(uuid.uuid4())
+
+    account = """
+                INSERT INTO accounts (account_id, name)
+                VALUES (?, ?)
+                ON CONFLICT (account_id)
+                DO UPDATE SET
+                    name=excluded.name
+                """
+
+    entry_data = (new_uuid, name)
+
+    cursor.execute(account, entry_data)
+    conn.commit()
+    conn.close()
